ca04f904ef8a9ecb83c45a0270278c03c1e168eb
Varon88 <nethan1226@gmail.com>
2023-09-23 12:31:32 +0530

    routing functionality

--- devicecomp-ui/src/App.js
+++ devicecomp-ui/src/App.js
@@ -2,15 +2,23 @@ import "./App.css";
 import FoooterComponent from "./Components/FoooterComponent";
 import HeaderComponent from "./Components/HeaderComponent";
 import LaptopListComponent from "./Components/LaptopListComponent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //used to establish routing; required to run command npm install react-router-dom
 
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <LaptopListComponent />
-      </div>
-      <FoooterComponent />
+      <Router>
+        <div className="container">
+          <HeaderComponent />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<LaptopListComponent />} />
+              <Route path="/getall" element={<LaptopListComponent />} />
+            </Routes>
+          </div>
+          <FoooterComponent />
+        </div>
+      </Router>
     </div>
   );
 }
