bdad10e00382de45d621a1e212c10559ac3d2598
Varon88 <nethan1226@gmail.com>
2023-09-25 00:38:13 +0530

    //

--- devicecomp-ui/src/Components/CreateLaptopComponent.jsx
+++ devicecomp-ui/src/Components/CreateLaptopComponent.jsx
@@ -1,7 +1,5 @@
 // import React, { Component } from 'react';
 
-
-
 // class CreateLaptopComponent extends Component {
 
 //     constructor(props){
@@ -32,7 +30,7 @@
 //         let laptop = {name: this.state.laptopName, storage: this.state.laptopStorage, manufacturer: this.state.laptopManufacturer, model: this.state.laptopModel, specs: this.state.laptopSpecs, price: this.state.laptopPrice, useCondition: this.state.laptopUseCondition, releaseDate: this.state.laptopReleaseDate}
 //         console.log('laptop =>' + JSON.stringify(laptop))
 //       }
-    
+
 //       changeLaptopNameHandler = (event) => {
 //         this.setState ({laptopName: event. target. value});
 //       }
@@ -57,7 +55,6 @@
 //       changeLaptopReleaseHandler = (event) => {
 //         this.setState ({laptopReleaseDate: event. target. value});
 //       }
-   
 
 //     render() {
 //         return (
@@ -70,42 +67,42 @@
 //                                 <form>
 //                                     <div className='form-group'>
 //                                         <label>Laptop Name: </label>
-//                                         <input placeholder='Laptop name' name='laptopName' className='form-control' 
+//                                         <input placeholder='Laptop name' name='laptopName' className='form-control'
 //                                             value={this.state.laptopName} onChange={this.changeLaptopNameHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop Storage: </label>
-//                                         <input placeholder='Laptop Storage' name='laptopStorage' className='form-control' 
+//                                         <input placeholder='Laptop Storage' name='laptopStorage' className='form-control'
 //                                             value={this.state.laptopStorage} onChange={this.changeLaptopStorageHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop manufacturer: </label>
-//                                         <input placeholder='Laptop manufacturer' name='laptopManufacturer' className='form-control' 
+//                                         <input placeholder='Laptop manufacturer' name='laptopManufacturer' className='form-control'
 //                                             value={this.state.laptopManufacturer} onChange={this.changeLaptopManufacturerHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop model: </label>
-//                                         <input placeholder='Laptop model' name='laptopModel' className='form-control' 
+//                                         <input placeholder='Laptop model' name='laptopModel' className='form-control'
 //                                             value={this.state.laptopModel} onChange={this.changeLaptopModelHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop specs: </label>
-//                                         <input placeholder='Laptop specs' name='laptopSpecs' className='form-control' 
+//                                         <input placeholder='Laptop specs' name='laptopSpecs' className='form-control'
 //                                             value={this.state.laptopSpecs} onChange={this.changeLaptopSpecsHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop price: </label>
-//                                         <input placeholder='Laptop price' name='laptopPrice' className='form-control' 
+//                                         <input placeholder='Laptop price' name='laptopPrice' className='form-control'
 //                                             value={this.state.laptopPrice} onChange={this.changeLaptopPriceHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop use conidtion: </label>
-//                                         <input placeholder='Laptop UseCondition' name='laptopUseCondition' className='form-control' 
+//                                         <input placeholder='Laptop UseCondition' name='laptopUseCondition' className='form-control'
 //                                             value={this.state.laptopUseCondition} onChange={this.changeLaptopConditionHandler} />
 //                                     </div>
 //                                     <div className='form-group'>
 //                                         <label>Laptop release date: </label>
-//                                         <input placeholder='Laptop ReleaseDate' name='laptopReleaseDate' className='form-control' 
+//                                         <input placeholder='Laptop ReleaseDate' name='laptopReleaseDate' className='form-control'
 //                                             value={this.state.laptopReleaseDate} onChange={this.changeLaptopReleaseHandler} />
 //                                     </div>
 //                                     <button className='btn btn-success' onClick={this.saveLaptop}>Save</button>
@@ -122,18 +119,18 @@
 
 // export default CreateLaptopComponent;
 
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreateLaptopComponent = () => {
-  const [laptopName, setLaptopName] = useState('');
-  const [laptopStorage, setLaptopStorage] = useState('');
-  const [laptopManufacturer, setLaptopManufacturer] = useState('');
-  const [laptopModel, setLaptopModel] = useState('');
-  const [laptopSpecs, setLaptopSpecs] = useState('');
-  const [laptopPrice, setLaptopPrice] = useState('');
-  const [laptopUseCondition, setLaptopUseCondition] = useState('');
-  const [laptopReleaseDate, setLaptopReleaseDate] = useState('');
+  const [laptopName, setLaptopName] = useState("");
+  const [laptopStorage, setLaptopStorage] = useState("");
+  const [laptopManufacturer, setLaptopManufacturer] = useState("");
+  const [laptopModel, setLaptopModel] = useState("");
+  const [laptopSpecs, setLaptopSpecs] = useState("");
+  const [laptopPrice, setLaptopPrice] = useState("");
+  const [laptopUseCondition, setLaptopUseCondition] = useState("");
+  const [laptopReleaseDate, setLaptopReleaseDate] = useState("");
   const navigate = useNavigate();
 
   const saveLaptop = (event) => {
@@ -151,110 +148,126 @@ const CreateLaptopComponent = () => {
     };
 
     // TODO: Save the laptop object to the database
-    console.log('laptop =>' + JSON.stringify(laptop));
+    console.log("laptop =>" + JSON.stringify(laptop));
   };
 
   const cancel = () => {
-     navigate('/laptop-getall');
-  }
+    navigate("/laptop-getall");
+  };
 
   return (
     <div>
-      <div className='container'>
-        <div className='row'>
+      <div className="container">
+        <div className="row">
           <div className='"card col-md-6 offset-md-3 offset-md-3'>
-            <h3 className='text-center'> Add Laptops</h3>
-            <div className='card-body'>
+            <h3 className="text-center"> Add Laptops</h3>
+            <div className="card-body">
               <form onSubmit={saveLaptop}>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop Name: </label>
                   <input
-                    placeholder='Laptop name'
-                    name='laptopName'
-                    className='form-control'
+                    placeholder="Laptop name"
+                    name="laptopName"
+                    className="form-control"
                     value={laptopName}
                     onChange={(event) => setLaptopName(event.target.value)}
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop Storage: </label>
                   <input
-                    placeholder='Laptop Storage'
-                    name='laptopStorage'
-                    className='form-control'
+                    placeholder="Laptop Storage"
+                    name="laptopStorage"
+                    className="form-control"
                     value={laptopStorage}
                     onChange={(event) => setLaptopStorage(event.target.value)}
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop manufacturer: </label>
                   <input
-                    placeholder='Laptop manufacturer'
-                    name='laptopManufacturer'
-                    className='form-control'
+                    placeholder="Laptop manufacturer"
+                    name="laptopManufacturer"
+                    className="form-control"
                     value={laptopManufacturer}
-                    onChange={(event) => setLaptopManufacturer(event.target.value)}
+                    onChange={(event) =>
+                      setLaptopManufacturer(event.target.value)
+                    }
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop model: </label>
                   <input
-                    placeholder='Laptop model'
-                    name='laptopModel'
-                    className='form-control'
+                    placeholder="Laptop model"
+                    name="laptopModel"
+                    className="form-control"
                     value={laptopModel}
                     onChange={(event) => setLaptopModel(event.target.value)}
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop specs: </label>
                   <input
-                    placeholder='Laptop specs'
-                    name='laptopSpecs'
-                    className='form-control'
+                    placeholder="Laptop specs"
+                    name="laptopSpecs"
+                    className="form-control"
                     value={laptopSpecs}
                     onChange={(event) => setLaptopSpecs(event.target.value)}
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop price: </label>
                   <input
-                    placeholder='Laptop price'
-                    name='laptopPrice'
-                    className='form-control'
+                    placeholder="Laptop price"
+                    name="laptopPrice"
+                    className="form-control"
                     value={laptopPrice}
                     onChange={(event) => setLaptopPrice(event.target.value)}
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop use conidtion: </label>
                   <input
-                    placeholder='Laptop UseCondition'
-                    name='laptopUseCondition'
-                    className='form-control'
+                    placeholder="Laptop UseCondition"
+                    name="laptopUseCondition"
+                    className="form-control"
                     value={laptopUseCondition}
-                    onChange={(event) => setLaptopUseCondition(event.target.value)}
+                    onChange={(event) =>
+                      setLaptopUseCondition(event.target.value)
+                    }
                   />
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>Laptop release date: </label>
                   <input
-                    placeholder='Laptop ReleaseDate'
-                    name='laptopReleaseDate'
-                    className='form-control'
+                    placeholder="Laptop ReleaseDate"
+                    name="laptopReleaseDate"
+                    className="form-control"
                     value={laptopReleaseDate}
-                    onChange={(event) => setLaptopReleaseDate(event.target.value)}
+                    onChange={(event) =>
+                      setLaptopReleaseDate(event.target.value)
+                    }
                   />
                 </div>
-                <button className='btn btn-success' onClick={saveLaptop}>Save</button>
-                <button className='btn btn-danger' onClick={cancel} style={{ marginLeft: '10px' }}>Cancel</button>
-                </form>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
+                <button className="btn btn-success" onClick={saveLaptop}>
+                  Save
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={cancel}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Cancel
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default CreateLaptopComponent;
+
+//TODO : look at the differnce between functional components and class components
