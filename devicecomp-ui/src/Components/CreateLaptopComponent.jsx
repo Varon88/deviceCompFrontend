3df84a1c842987289f5dda68c208513218bd1b9c
Varon88 <nethan1226@gmail.com>
2023-09-25 00:37:11 +0530

    functional version of createlaptop component with the form added

--- devicecomp-ui/src/Components/CreateLaptopComponent.jsx
+++ devicecomp-ui/src/Components/CreateLaptopComponent.jsx
@@ -1,24 +1,260 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
 
 
-class CreateLaptopComponent extends Component {
+// class CreateLaptopComponent extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-          laptops: [],
-        };
-      }
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//           name: '',
+//           storage: '',
+//           manufacturer: '',
+//           model: '',
+//           specs: '',
+//           price: '',
+//           useCondition: '',
+//           releaseDate: ''
+//         };
+//         this.changeLaptopNameHandler = this.changeLaptopNameHandler.bind(this);
+//         this.changeLaptopStorageHandler = this.changeLaptopStorageHandler.bind(this);
+//         this.changeLaptopManufacturerHandler = this.changeLaptopManufacturerHandler.bind(this);
+//         this.changeLaptopModelHandler = this.changeLaptopModelHandler.bind(this);
+//         this.changeLaptopSpecsHandler = this.changeLaptopSpecsHandler.bind(this);
+//         this.changeLaptopPriceHandler = this.changeLaptopPriceHandler.bind(this);
+//         this.changeLaptopConditionHandler = this.changeLaptopConditionHandler.bind(this);
+//         this.changeLaptopReleaseHandler = this.changeLaptopReleaseHandler.bind(this);
+//         this.saveLaptop = this.saveLaptop.bind(this);
+//       }
+
+//       saveLaptop = (laptop) => {
+//         laptop.preventDefault();
+//         let laptop = {name: this.state.laptopName, storage: this.state.laptopStorage, manufacturer: this.state.laptopManufacturer, model: this.state.laptopModel, specs: this.state.laptopSpecs, price: this.state.laptopPrice, useCondition: this.state.laptopUseCondition, releaseDate: this.state.laptopReleaseDate}
+//         console.log('laptop =>' + JSON.stringify(laptop))
+//       }
     
+//       changeLaptopNameHandler = (event) => {
+//         this.setState ({laptopName: event. target. value});
+//       }
+//       changeLaptopStorageHandler = (event) => {
+//         this.setState ({laptopStorage: event. target. value});
+//       }
+//       changeLaptopManufacturerHandler = (event) => {
+//         this.setState ({laptopManufacturer: event. target. value});
+//       }
+//       changeLaptopModelHandler = (event) => {
+//         this.setState ({laptopModel: event. target. value});
+//       }
+//       changeLaptopSpecsHandler = (event) => {
+//         this.setState ({laptopSpecs: event. target. value});
+//       }
+//       changeLaptopPriceHandler = (event) => {
+//         this.setState ({laptopPrice: event. target. value});
+//       }
+//       changeLaptopConditionHandler = (event) => {
+//         this.setState ({laptopUseCondition: event. target. value});
+//       }
+//       changeLaptopReleaseHandler = (event) => {
+//         this.setState ({laptopReleaseDate: event. target. value});
+//       }
+   
 
-    render() {
-        return (
-            <div>
-                <h1> Laptop Form </h1>
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <div className='container'>
+//                     <div className='row'>
+//                         <div className='"card col-md-6 offset-md-3 offset-md-3'>
+//                             <h3 className='text-center'> Add Laptops</h3>
+//                             <div className='card-body'>
+//                                 <form>
+//                                     <div className='form-group'>
+//                                         <label>Laptop Name: </label>
+//                                         <input placeholder='Laptop name' name='laptopName' className='form-control' 
+//                                             value={this.state.laptopName} onChange={this.changeLaptopNameHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop Storage: </label>
+//                                         <input placeholder='Laptop Storage' name='laptopStorage' className='form-control' 
+//                                             value={this.state.laptopStorage} onChange={this.changeLaptopStorageHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop manufacturer: </label>
+//                                         <input placeholder='Laptop manufacturer' name='laptopManufacturer' className='form-control' 
+//                                             value={this.state.laptopManufacturer} onChange={this.changeLaptopManufacturerHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop model: </label>
+//                                         <input placeholder='Laptop model' name='laptopModel' className='form-control' 
+//                                             value={this.state.laptopModel} onChange={this.changeLaptopModelHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop specs: </label>
+//                                         <input placeholder='Laptop specs' name='laptopSpecs' className='form-control' 
+//                                             value={this.state.laptopSpecs} onChange={this.changeLaptopSpecsHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop price: </label>
+//                                         <input placeholder='Laptop price' name='laptopPrice' className='form-control' 
+//                                             value={this.state.laptopPrice} onChange={this.changeLaptopPriceHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop use conidtion: </label>
+//                                         <input placeholder='Laptop UseCondition' name='laptopUseCondition' className='form-control' 
+//                                             value={this.state.laptopUseCondition} onChange={this.changeLaptopConditionHandler} />
+//                                     </div>
+//                                     <div className='form-group'>
+//                                         <label>Laptop release date: </label>
+//                                         <input placeholder='Laptop ReleaseDate' name='laptopReleaseDate' className='form-control' 
+//                                             value={this.state.laptopReleaseDate} onChange={this.changeLaptopReleaseHandler} />
+//                                     </div>
+//                                     <button className='btn btn-success' onClick={this.saveLaptop}>Save</button>
+//                                     <button className='btn btn-danger' onClick={this.cancel.bind(this)} style={{ marginLeft: '10px' }}>Cancel</button>
+//                                 </form>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// export default CreateLaptopComponent;
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CreateLaptopComponent = () => {
+  const [laptopName, setLaptopName] = useState('');
+  const [laptopStorage, setLaptopStorage] = useState('');
+  const [laptopManufacturer, setLaptopManufacturer] = useState('');
+  const [laptopModel, setLaptopModel] = useState('');
+  const [laptopSpecs, setLaptopSpecs] = useState('');
+  const [laptopPrice, setLaptopPrice] = useState('');
+  const [laptopUseCondition, setLaptopUseCondition] = useState('');
+  const [laptopReleaseDate, setLaptopReleaseDate] = useState('');
+  const navigate = useNavigate();
+
+  const saveLaptop = (event) => {
+    event.preventDefault();
+
+    const laptop = {
+      name: laptopName,
+      storage: laptopStorage,
+      manufacturer: laptopManufacturer,
+      model: laptopModel,
+      specs: laptopSpecs,
+      price: laptopPrice,
+      useCondition: laptopUseCondition,
+      releaseDate: laptopReleaseDate,
+    };
+
+    // TODO: Save the laptop object to the database
+    console.log('laptop =>' + JSON.stringify(laptop));
+  };
+
+  const cancel = () => {
+     navigate('/laptop-getall');
+  }
+
+  return (
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <div className='"card col-md-6 offset-md-3 offset-md-3'>
+            <h3 className='text-center'> Add Laptops</h3>
+            <div className='card-body'>
+              <form onSubmit={saveLaptop}>
+                <div className='form-group'>
+                  <label>Laptop Name: </label>
+                  <input
+                    placeholder='Laptop name'
+                    name='laptopName'
+                    className='form-control'
+                    value={laptopName}
+                    onChange={(event) => setLaptopName(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop Storage: </label>
+                  <input
+                    placeholder='Laptop Storage'
+                    name='laptopStorage'
+                    className='form-control'
+                    value={laptopStorage}
+                    onChange={(event) => setLaptopStorage(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop manufacturer: </label>
+                  <input
+                    placeholder='Laptop manufacturer'
+                    name='laptopManufacturer'
+                    className='form-control'
+                    value={laptopManufacturer}
+                    onChange={(event) => setLaptopManufacturer(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop model: </label>
+                  <input
+                    placeholder='Laptop model'
+                    name='laptopModel'
+                    className='form-control'
+                    value={laptopModel}
+                    onChange={(event) => setLaptopModel(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop specs: </label>
+                  <input
+                    placeholder='Laptop specs'
+                    name='laptopSpecs'
+                    className='form-control'
+                    value={laptopSpecs}
+                    onChange={(event) => setLaptopSpecs(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop price: </label>
+                  <input
+                    placeholder='Laptop price'
+                    name='laptopPrice'
+                    className='form-control'
+                    value={laptopPrice}
+                    onChange={(event) => setLaptopPrice(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop use conidtion: </label>
+                  <input
+                    placeholder='Laptop UseCondition'
+                    name='laptopUseCondition'
+                    className='form-control'
+                    value={laptopUseCondition}
+                    onChange={(event) => setLaptopUseCondition(event.target.value)}
+                  />
+                </div>
+                <div className='form-group'>
+                  <label>Laptop release date: </label>
+                  <input
+                    placeholder='Laptop ReleaseDate'
+                    name='laptopReleaseDate'
+                    className='form-control'
+                    value={laptopReleaseDate}
+                    onChange={(event) => setLaptopReleaseDate(event.target.value)}
+                  />
+                </div>
+                <button className='btn btn-success' onClick={saveLaptop}>Save</button>
+                <button className='btn btn-danger' onClick={cancel} style={{ marginLeft: '10px' }}>Cancel</button>
+                </form>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+  );
+};
 
 export default CreateLaptopComponent;
--- devicecomp-ui/src/Components/LaptopListComponent.jsx
+++ devicecomp-ui/src/Components/LaptopListComponent.jsx
@@ -1,74 +1,76 @@
 //react snippets are to be used.
 // a http call has not been made in this situation; axios library is to be used. This is installed using the terminal using command npm install axios --save
 //react class component snippet that returns its skeleton [rcc]
-import React, { Component } from "react";
-import LaptopService from "../Services/LaptopService";
+// import React, { Component, useState, useNavigate } from "react";
+// import LaptopService from "../Services/LaptopService";
 
 
-class LaptopListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      laptops: [],
-    }
-    this.addLaptop = this.addLaptop.bind(this);
-  }
+// class LaptopListComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       laptops: [],
+//     }
+//     this.addLaptop = this.addLaptop.bind(this);
+//   }
 
-  //every time a component is mounted with a service this should be specified.
-  componentDidMount() {
-    LaptopService.getLaptops().then((res) => {
-      this.setState({ laptops: res.data });
-      //above 2 lines initiates the getlaptop function which has a response and then assigns each value in to the laptops array
-    });
-  }
+//   //every time a component is mounted with a service this should be specified.
+//   componentDidMount() {
+//     LaptopService.getLaptops().then((res) => {
+//       this.setState({ laptops: res.data });
+//       //above 2 lines initiates the getlaptop function which has a response and then assigns each value in to the laptops array
+//     });
+//   }
 
-  addLaptop(){
-    this.props.history.push('/laptop-add')
-  }
+//   addLaptop(){
+//     // this.props.history.push('/laptop-add')
+//     const navigate = useNavigate();
+//     navigate('/laptop-add');
+//   }
 
-  render() {
-    return (
-      <div>
-        <h2 className="text-center">Laptop List</h2>
-        {/* check what this classname is used for--> */}
-        <div className="row">
-          <button className="btn btn-primary" onClick={this.addLaptop}> Add Laptops </button>
-          <table className="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th>laptopName</th>
-                <th>laptopStorage</th>
-                <th>laptopManufacturer</th>
-                <th>laptopModel</th>
-                <th>laptopSpecs</th>
-                <th>laptopPrice</th>
-                <th>laptopUseCondition</th>
-                <th>laptopReleaseDate</th>
-              </tr>
-            </thead>
+//   render() {
+//     return (
+//       <div>
+//         <h2 className="text-center">Laptop List</h2>
+//         {/* check what this classname is used for--> */}
+//         <div className="row">
+//           <button className="btn btn-primary" onClick={this.addLaptop}> Add Laptops </button>
+//           <table className="table table-striped table-bordered">
+//             <thead>
+//               <tr>
+//                 <th>laptopName</th>
+//                 <th>laptopStorage</th>
+//                 <th>laptopManufacturer</th>
+//                 <th>laptopModel</th>
+//                 <th>laptopSpecs</th>
+//                 <th>laptopPrice</th>
+//                 <th>laptopUseCondition</th>
+//                 <th>laptopReleaseDate</th>
+//               </tr>
+//             </thead>
 
-            <tbody>
-              {this.state.laptops.map((laptops) => (
-                <tr key={laptops.id}>
-                  <td>{laptops.name}</td>
-                  <td>{laptops.storage}</td>
-                  <td>{laptops.manufacturer}</td>
-                  <td>{laptops.model}</td>
-                  <td>{laptops.specs}</td>
-                  <td>{laptops.price}</td>
-                  <td>{laptops.useCondition}</td>
-                  <td>{laptops.releaseDate}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
-}
+//             <tbody>
+//               {this.state.laptops.map((laptops) => (
+//                 <tr key={laptops.id}>
+//                   <td>{laptops.name}</td>
+//                   <td>{laptops.storage}</td>
+//                   <td>{laptops.manufacturer}</td>
+//                   <td>{laptops.model}</td>
+//                   <td>{laptops.specs}</td>
+//                   <td>{laptops.price}</td>
+//                   <td>{laptops.useCondition}</td>
+//                   <td>{laptops.releaseDate}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default LaptopListComponent;
+// export default LaptopListComponent;
 
 /* In a React component's constructor, `props` is an object that represents the properties or props that are passed to the component when it's created. Props allow you to pass data from a parent component to a child component. 
 
@@ -81,3 +83,69 @@ Here's a breakdown of what the code you provided does:
 3. `this.state = { laptops: [] };`: This line initializes the component's state. In React, the `state` is used to store and manage component-specific data that can change over time. In this case, it initializes a state property named `laptops` as an empty array. You can use this state property to store and update data that the component needs to render.
 
 Overall, this constructor is preparing the component to receive and manage data passed via its props and initializes an empty array in its state to store the `laptops` data. Typically, you would fetch data and update the `laptops` state within the component's lifecycle methods or in response to user interactions.*/
+
+//the above version of code is non functional due to the fact that it a class component and not a functional component hence not being able to use the useNavigate function which is the new version of the "this.props.history-push('/add-employee');".
+
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LaptopService from "../Services/LaptopService";
+
+const LaptopListComponent = () => {
+  const [laptops, setLaptops] = useState([]);
+  const navigate = useNavigate();
+
+
+  const addLaptop = () => {
+    navigate('/laptop-add');
+  };
+
+  React.useEffect(() => {
+    LaptopService.getLaptops().then((res) => {
+      setLaptops(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">Laptop List</h2>
+
+      <div className="row">
+        <button className="btn btn-primary" onClick={addLaptop}>
+          Add Laptops
+        </button>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>laptopName</th>
+              <th>laptopStorage</th>
+              <th>laptopManufacturer</th>
+              <th>laptopModel</th>
+              <th>laptopSpecs</th>
+              <th>laptopPrice</th>
+              <th>laptopUseCondition</th>
+              <th>laptopReleaseDate</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {laptops.map((laptops) => (
+              <tr key={laptops.id}>
+                <td>{laptops.name}</td>
+                <td>{laptops.storage}</td>
+                <td>{laptops.manufacturer}</td>
+                <td>{laptops.model}</td>
+                <td>{laptops.specs}</td>
+                <td>{laptops.price}</td>
+                <td>{laptops.useCondition}</td>
+                <td>{laptops.releaseDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default LaptopListComponent;
